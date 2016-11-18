f09f33a6ddf0cf397838068e9cc3bc82464bf699
paopow <ksiangliulue@gmail.com>
2016-11-18 10:03:49 -0500

    Add a scaffold for spiders to crawl annotations and images

--- labelme/labelme/spiders/__init__.py
+++ labelme/labelme/spiders/__init__.py
@@ -2,3 +2,29 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+import scrapy
+
+ANNOTATION_URL = 'http://people.csail.mit.edu/brussell/research/LabelMe/Annotations/'
+IMG_URL = 'http://people.csail.mit.edu/brussell/research/LabelMe/Images/'
+
+
+class AnnotationSpider(scrapy.Spider):
+    name = 'annotations'
+    start_urls = [ANNOTATION_URL]
+
+    def parse_annotation(self, response):
+        pass
+
+    def parse(self, response):
+        pass
+
+
+class ImageSpider(scrapy.Spider):
+    name = 'images'
+    start_urls = [IMG_URL]
+
+    def parse_image(self, response):
+        pass
+
+    def parse(self, response):
+        pass
